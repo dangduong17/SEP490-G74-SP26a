e5478fc3f0d4b5f286474c6da9940ad9f31127aa
duongdang <duongdmhe141178@fpt.edu.vn>
2026-02-27 01:36:45 +0700

    innit base

---
+++ RJMS/vn/edu/fpt/model/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RJMS.Vn.Edu.Fpt.Model;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
