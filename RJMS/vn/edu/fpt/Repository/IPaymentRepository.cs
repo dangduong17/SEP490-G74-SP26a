b95158133fed0ff86776546b101f3a2129247229
anhdnthe171790 <anhdnthe171790@fpt.edu.vn>
2026-03-10 23:29:13 +0700

    anhdnthe171790-payment
    anhdnthe171790 payment

---
+++ RJMS/vn/edu/fpt/Repository/IPaymentRepository.cs
@@ -0,0 +1,19 @@
+using RJMS.vn.edu.fpt.Models;
+
+namespace RJMS.Vn.Edu.Fpt.Repository
+{
+    public interface IPaymentRepository
+    {
+        Task<List<SubscriptionPlan>> GetActiveSubscriptionPlansAsync();
+        Task<SubscriptionPlan?> GetSubscriptionPlanByIdAsync(int planId);
+        Task<int> CreateSubscriptionAsync(int userId, int planId);
+        Task<int> CreatePaymentAsync(int subscriptionId, decimal amount);
+        Task<bool> UpdatePaymentStatusAsync(int paymentId, string status, string transactionId);
+        Task<bool> UpdateSubscriptionStatusAsync(int subscriptionId, string status);
+        Task<bool> CreateSubscriptionPeriodAsync(int subscriptionId, int planId, DateTime startDate, DateTime endDate);
+        Task<int> CreateInvoiceAsync(Invoice invoice);
+        Task<Payment?> GetPaymentByIdAsync(int paymentId);
+        Task<Subscription?> GetSubscriptionByIdAsync(int subscriptionId);
+        Task<User?> GetUserByIdAsync(int userId);
+    }
+}
