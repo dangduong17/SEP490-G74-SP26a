a99f36b9c66634a27b060cf62b694ee50b81a454
duongdmhe141178 <duongdmhe141178@fpt.edu.vn>
2026-03-23 21:57:22 +0700

    duongdmhe141178-implement-cv
    duongdmhe141178-implement-cv

--- RJMS/vn/edu/fpt/Models/Cv.cs
+++ RJMS/vn/edu/fpt/Models/Cv.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace RJMS.vn.edu.fpt.Models;
@@ -6,18 +6,29 @@ namespace RJMS.vn.edu.fpt.Models;
 public partial class Cv
 {
     public int Id { get; set; }
-
     public int CandidateId { get; set; }
-
     public string? Title { get; set; }
 
-    public string? FilePath { get; set; }
+    /// <summary>UPLOAD | BUILDER</summary>
+    public string CvType { get; set; } = "UPLOAD";
 
-    public int? ViewCount { get; set; }
+    // Upload fields
+    public string? LegacyFilePath { get; set; }
+    public string? FileUrl { get; set; }
+    public string? FileName { get; set; }
+    public int? FileSize { get; set; }
 
+    // Builder fields
+    public int? TemplateId { get; set; }
+
+    public bool IsDefault { get; set; } = false;
+    public int? ViewCount { get; set; } = 0;
     public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 
-    public virtual ICollection<Application> Applications { get; set; } = new List<Application>();
-
+    // Nav props
     public virtual Candidate Candidate { get; set; } = null!;
+    public virtual CvTemplate? Template { get; set; }
+    public virtual CvData? CvData { get; set; }
+    public virtual ICollection<Application> Applications { get; set; } = new List<Application>();
 }
