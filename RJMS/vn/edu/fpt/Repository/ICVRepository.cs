e23eecc194ccd7deabe18afc81f41791dbf6c3f8
anhdnthe171790 <anhdnthe171790@fpt.edu.vn>
2026-03-23 22:08:09 +0700

    anhdnthe171790-fix-view
    anhdnthe171790-fix-view

--- RJMS/vn/edu/fpt/Repository/ICVRepository.cs
+++ RJMS/vn/edu/fpt/Repository/ICVRepository.cs
@@ -6,7 +6,33 @@ namespace RJMS.Vn.Edu.Fpt.Repository
 {
     public interface ICVRepository
     {
-        Task<List<Cv>> GetCvsByCandidateIdAsync(int candidateId);
         Task<Candidate?> GetCandidateByUserIdAsync(int userId);
+
+        // CV CRUD
+        Task<List<Cv>> GetCvsByCandidateIdAsync(int candidateId);
+        Task<Cv?> GetCvByIdAsync(int cvId);
+        Task<Cv> CreateCvAsync(Cv cv);
+        Task UpdateCvAsync(Cv cv);
+        Task DeleteCvAsync(Cv cv);
+
+        // CvData (builder JSON)
+        Task<CvData?> GetCvDataByCvIdAsync(int cvId);
+        Task<CvData> CreateCvDataAsync(CvData data);
+        Task UpdateCvDataAsync(CvData data);
+
+        // Templates
+        Task<List<CvTemplate>> GetActiveTemplatesAsync();
+        Task<List<CvTemplate>> GetAllTemplatesAsync();
+        Task<CvTemplate?> GetTemplateByIdAsync(int id);
+        Task<CvTemplate> CreateTemplateAsync(CvTemplate template);
+        Task UpdateTemplateAsync(CvTemplate template);
+        Task DeleteTemplateAsync(CvTemplate template);
+
+        // Template Categories
+        Task<List<TemplateCategory>> GetAllCategoriesAsync();
+        Task<TemplateCategory?> GetCategoryByIdAsync(int id);
+        Task<TemplateCategory> CreateCategoryAsync(TemplateCategory category);
+        Task UpdateCategoryAsync(TemplateCategory category);
+        Task DeleteCategoryAsync(TemplateCategory category);
     }
 }
