493d452ab4ab493c0128d92feed4a75824ed71b9
dungdqhe140879 <dungdqhe140879@fpt.edu.vn>
2026-03-24 16:26:03 +0700

    dungdqhe140879-delete-candidate
    dungdqhe140879-delete-candidate

--- RJMS/vn/edu/fpt/Service/ICandidateDashboardService.cs
+++
@@ -1,9 +0,0 @@
-using RJMS.Vn.Edu.Fpt.Model.DTOs;
-
-namespace RJMS.Vn.Edu.Fpt.Service
-{
-    public interface ICandidateDashboardService
-    {
-        Task<CandidateDashboardDTO> GetDashboardAsync(Guid userId);
-    }
-}
