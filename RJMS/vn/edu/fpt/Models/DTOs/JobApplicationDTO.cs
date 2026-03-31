f977b0ee40d68fdc68c7b583f59492a3d3daf49e
anhndhe180067 <anhndhe180067@fpt.edu.vn>
2026-03-31 22:16:27 +0700

    anhndhe180067-job-fix
    anhndhe180067-job-fix

--- RJMS/vn/edu/fpt/Models/DTOs/JobApplicationDTO.cs
+++ RJMS/vn/edu/fpt/Models/DTOs/JobApplicationDTO.cs
@@ -5,7 +5,11 @@ namespace RJMS.Vn.Edu.Fpt.Model.DTOs
     public class JobApplicationDTO
     {
         public int Id { get; set; }
+        public int JobId { get; set; }
         public string PositionTitle { get; set; } = string.Empty;
+        public string CustomTitle { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string? CompanyLogo { get; set; }
         public string Status { get; set; } = string.Empty;
         public DateTime AppliedAt { get; set; }
     }
